e559403a05b62e0f3c7a72998f10c091b56379f7
Some Guy <somguy@random.la>
2016-02-20 13:07:24 -0800

    End of video 02

---
+++ Web/Models/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web.Models
+{
+
+	[Table("people")]
+	public class Person
+	{
+		public Person ()
+		{
+		}
+
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Column("first_name")]
+		[Required(ErrorMessage = "First name is required")]
+		public string FirstName { get; set; }
+
+		[Column("last_name")]
+		[Required(ErrorMessage = "Last name is required")]
+		public string LastName { get; set; }
+
+		public string PhoneNumber { get; set; }
+
+		[Column("birth_date")]
+		[Required(ErrorMessage = "Birthday is required")]
+		public DateTime? BirthDate { get; set; }
+
+	}
+}
+
---
+++ Web/Scripts/sql/build_database.sql
@@ -0,0 +1,23 @@
+﻿
+mysql -uusername -ppassword
+
+DROP DATABASE IF EXISTS mvc5;
+CREATE DATABASE mvc5;
+USE mvc5;
+
+use mvc5;
+DROP TABLE IF EXISTS people;
+CREATE TABLE people (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  first_name varchar(20) DEFAULT NULL,
+  last_name varchar(20) DEFAULT NULL,
+  birth_date date DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+insert into people (first_name) values ('SomeGuy');
+insert into people (first_name,last_name,birth_date) values ('Some', 'Guy','0001-01-01');
+insert into people (first_name,last_name,birth_date) values ('Some', 'Guy','0001-01-01');
+
+use mvc5;
+select * from people;
